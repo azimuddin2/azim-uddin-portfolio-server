1a68022877583eea658d2a079011fd00f345a4af
azimuddin2 <mohammadazimuddin274@gmail.com>
2024-04-24 08:54:03 +0600

    database connection functionlity

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = 5000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+// database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.line7or.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
